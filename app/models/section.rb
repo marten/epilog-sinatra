ab52062dae8bdff9cd7de9602c7a0b43ec7d3c99
Marten Veldthuis <marten@veldthuis.com>
2010-09-25 11:15:20 +0200

    fix section

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,22 +1,24 @@
-class Blog
+class Section
   
   def initialize(site)
     @site = site
   end
   
-  def synchronize
-    metadata = @site.dropbox.metadata('/Site/#{slug}')
-    if dropbox_hash != metadata.hash
-      posts = self.posts.all
-      files = metadata.contents
-      
-      files.each do |file|
-        post = posts.find {|i| i.path == file.path }
-        if post
-          next if post.dropbox_modified == file.modified
-          post.synchronize!
-        else
-          self.posts.create_from_path()
-    end
-  end
+  # def synchronize
+  #   metadata = @site.dropbox.metadata('/Site/#{slug}')
+  #   if dropbox_hash != metadata.hash
+  #     posts = self.posts.all
+  #     files = metadata.contents
+  #     
+  #     files.each do |file|
+  #       post = posts.find {|i| i.path == file.path }
+  #       if post
+  #         next if post.dropbox_modified == file.modified
+  #         post.synchronize!
+  #       else
+  #         self.posts.create_from_path()
+  #       end
+  #     end
+  #   end
+  # end
 end
