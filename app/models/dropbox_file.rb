6532d0928650ff69c41032e746d9c663d233d474
Marten Veldthuis <marten@veldthuis.com>
2010-09-25 13:54:53 +0200

    add polymorphic association between files and "items", and callbacks to keep them up to date

--- app/models/dropbox_file.rb
+++ app/models/dropbox_file.rb
@@ -1,8 +1,24 @@
 class DropboxFile < ActiveRecord::Base
   belongs_to :dropbox_directory
+  
+  belongs_to :item, :polymorphic => true
+  
   before_save do
     if text_file? and (new_record? or date_changed?)
       self.contents = dropbox_directory.site.dropbox.download(path)
+    end
+  end
+  
+  after_save(:on => :create) do
+    # TODO figure out what type of item to make, and make it
+    if path =~ /^\/Site\/blog\//
+      Post.create(:site => dropbox_directory.site, :dropbox_file => self)
+    end
+  end
+  
+  after_save(:on => :update) do
+    if item and item.respond_to?(:process_dropbox_file_update)
+      item.process_dropbox_file_update(self)
     end
   end
   
@@ -10,4 +26,5 @@ class DropboxFile < ActiveRecord::Base
     # FIXME This is a really simple check, we need to do better than that. MIME::Types perhaps?
     path =~ /\.(txt|markdown|textile|html|css)$/
   end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :site
   
+  has_one :dropbox_file, :as => :item
+  
   def self.sync
     site.dropbox.metadata('Site/blog')
   end
